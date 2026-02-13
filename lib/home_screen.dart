ed331b871662e5bc8e5b813e819823a08c47cb93
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-02-14 00:17:55 +0500

    Login Page Practice

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-
-class HomeScreen extends StatefulWidget 
-{
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
@@ -10,34 +8,105 @@ class HomeScreen extends StatefulWidget
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
-  Widget build(BuildContext context) 
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
-      
-      appBar: AppBar 
-      (
-        
-        title: const Text('Home Screen', ),
-        backgroundColor: Colors.blue,
-        
-        
-      ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image(
-              image: AssetImage("assets/image1.jpg"),
-              height: 200,),
-            CircleAvatar(
-              
-              radius: 100,
-              backgroundImage: AssetImage("assets/image1.jpg"),
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: formKey,
+          child: ListView(
+            children: [
+              FlutterLogo(size: 100),
+              SizedBox(height: 20),
+              Text(
+                "Welcome Back!",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  hintText: 'Enter Your Email',
+                  prefixIcon: Icon(Icons.email),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                controller: email,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  if (!value.contains('@') || !value.contains('.')) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  hintText: 'Enter Your Password',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                controller: password,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be at least 6 characters';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+
+              SizedBox(
+                width: double.infinity,
+
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.indigo,
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.all(10),
+                  ),
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {}
+                  },
+                  child: Text('Login'),
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: () {}, child: Text('Forgot Password?')),
+                ],
+              ),
+              SizedBox(height: 10),
+
+              SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: Text('Don\'t have an account? Sign Up'),
+                ),
+              ),
+            ],
+          ),
         ),
-      )
+      ),
     );
   }
 }
