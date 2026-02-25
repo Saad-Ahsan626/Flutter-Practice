5129899784cde244f203ea429dfed1bf1ca41dd3
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-02-25 23:36:54 +0500

    Firebase Installation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/firebase_options.dart';
 import 'package:flutter_practice/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MyApp());
 }
 
